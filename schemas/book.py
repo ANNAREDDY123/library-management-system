6f6a2e461be40e90ec2189b57055614cf380b3e3
Annareddy Jagadeswar Reddy <100749580+ANNAREDDY123@users.noreply.github.com>
2026-06-30 11:45:54 +0530

    Create book.py

---
+++ schemas/book.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+
+class BookCreate(BaseModel):
+
+    title: str
+
+    author: str
+
+    category: str
+
+    isbn: str
+
+    total_copies: int
+
+    available_copies: int
+
+    is_active: bool = True
